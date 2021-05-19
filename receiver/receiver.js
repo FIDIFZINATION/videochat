be000d096ced1a8937f90432848f67e523c0c0f9
fidifzi nation <69453680+FIDIFZINATION@users.noreply.github.com>
2021-05-20 00:38:19 +0530

    Add files via upload

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -1,24 +1,107 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="../style.css">
-    </head>
-    <body>
-        <div>
-            <input placeholder="Enter username..."
-                    type="text"
-                    id="username-input" /><br>
-            <button onclick="joinCall()">Join Call</button>
-        </div>
-        <div id="video-call-div">
-            <video muted id="local-video" autoplay></video>
-            <video id="remote-video" autoplay></video>
-            <div class="call-action-div">
-                <button onclick="muteVideo()">Mute Video</button>
-                <button onclick="muteAudio()">Mute Audio</button>
-            </div>
-        </div>
-        <script src="./receiver.js"></script>
-    </body>
+const webSocket = new WebSocket("ws://127.0.0.1:3000")
 
-</html>
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "offer":
+            peerConn.setRemoteDescription(data.offer)
+            createAndSendAnswer()
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+function createAndSendAnswer () {
+    peerConn.createAnswer((answer) => {
+        peerConn.setLocalDescription(answer)
+        sendData({
+            type: "send_answer",
+            answer: answer
+        })
+    }, error => {
+        console.log(error)
+    })
+}
+
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+let username
+
+function joinCall() {
+
+    username = document.getElementById("username-input").value
+
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
+                }
+            ]
+        }
+
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            
+            sendData({
+                type: "send_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        sendData({
+            type: "join_call"
+        })
+
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
+}
